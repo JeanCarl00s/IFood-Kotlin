0ac9882ccc82998b0abb9387115edf79b9d29533
JeanCarl00s <devjeanfernandes@gmail.com>
2022-01-14 17:46:23 -0300

    categoryList

--- app/src/main/java/com/jean/ifood/RestaurantFragment.kt
+++ app/src/main/java/com/jean/ifood/RestaurantFragment.kt
@@ -2,12 +2,19 @@ package com.jean.ifood
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.tiagoaguiar.atway.ui.adapter.ATAdapter
 import com.jean.ifood.databinding.FragmentRestaurantBinding
 
 class RestaurantFragment : Fragment(R.layout.fragment_restaurant) {
 
     private var binding: FragmentRestaurantBinding? = null
+
+    private var categoryAdapter = ATAdapter( {
+        CategoryView(it)
+    })
 
     private var filters = arrayOf(
         FilterItem(1,"Ordenar", closeIcon = R.drawable.ic_baseline_keyboard_arrow_down_24),
@@ -23,9 +30,21 @@ class RestaurantFragment : Fragment(R.layout.fragment_restaurant) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        categoryAdapter.items = arrayListOf(
+            Category(1, "https://www.ifood.com.br/static/images/categories/market.png", "mercado", 0xFFB6D048),
+            Category(2, "https://www.ifood.com.br/static/images/categories/restaurant.png", "Restaurante", 0xFFE91D2D),
+            Category(3, "https://www.ifood.com.br/static/images/categories/drinks.png", "Bebidas", 0xFFF6D553),
+            Category(4, "https://static-images.ifood.com.br/image/upload/f_auto/webapp/landingV2/express.png", "Express", 0xFFF0000),
+            Category(5, "https://parceiros.ifood.com.br/static/media/salad.9db040c0.png", "Saudavel", 0xFFE91D2D),
+            Category(6, "https://www.ifood.com.br/static/images/categories/drinks.png", "Salgados", 0xFF8C60C5),
+        )
+
         binding = FragmentRestaurantBinding.bind(view)
 
         binding?.let {
+            it.rvCategory.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            it.rvCategory.adapter = categoryAdapter
+
             filters.forEach { filter ->
                 it.chipGroupFilter.addView(filter.toChip(requireContext()))
             }
